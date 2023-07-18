0cd4ee75c89df98fba28eb3378e23d70c9118893
prathameshadam10 <prathameshadam10@gmail.com>
2023-07-18 14:46:44 +0530

    added:UC_1_Check_Emp_Present_Absent

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -2,5 +2,11 @@ import random
 
 if __name__ == '__main__':
     print("Welcome to Employee Wage Computation Program")
+    emp_full_time = 1
 
+    emp_check = random.randint(0, 1)
+    if emp_check == emp_full_time:
+        print("Employee Present")
+    else:
+        print("Employee Absent")
 
