026c825dfc4823a15c283bbcb588ae06cc19b21b
prathameshadam10 <prathameshadam10@gmail.com>
2023-07-18 14:58:58 +0530

    added:UC_2_Daily_emp_wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,10 +3,17 @@ import random
 if __name__ == '__main__':
     print("Welcome to Employee Wage Computation Program")
     emp_full_time = 1
+    emp_wage_per_hour = 20
+    emp_hrs = 0
+    daily_emp_wage = 0
 
     emp_check = random.randint(0, 1)
     if emp_check == emp_full_time:
+        emp_hrs = 8
         print("Employee Present")
     else:
+        emp_hrs = 0
         print("Employee Absent")
+    daily_emp_wage = emp_hrs * emp_wage_per_hour
+    print('Emp Wage : ' + str(daily_emp_wage))
 
