14e9c3c6cfd399dfe44eeb032ec94143dbecfdf3
prathameshadam10 <prathameshadam10@gmail.com>
2023-07-18 15:06:11 +0530

    added:UC_3_part_time_wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,14 +3,18 @@ import random
 if __name__ == '__main__':
     print("Welcome to Employee Wage Computation Program")
     emp_full_time = 1
+    emp_part_time = 2
     emp_wage_per_hour = 20
     emp_hrs = 0
     daily_emp_wage = 0
 
-    emp_check = random.randint(0, 1)
+    emp_check = random.randint(0, 2)
     if emp_check == emp_full_time:
         emp_hrs = 8
-        print("Employee Present")
+        print("Employee Full-time Present")
+    elif emp_check == emp_part_time:
+        emp_hrs = 4
+        print("Employee Part-time Present")
     else:
         emp_hrs = 0
         print("Employee Absent")
