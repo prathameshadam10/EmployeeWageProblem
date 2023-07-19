fdb77511aefe445fb7a56f461344bd0d43b60982
prathameshadam10 <prathameshadam10@gmail.com>
2023-07-19 16:32:25 +0530

    Added:Functions

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,12 +1,16 @@
 import random
 
-if __name__ == '__main__':
-    print("Welcome to Employee Wage Computation Program")
-    emp_full_time = 1
+
+def check_emp_present_absent():
+    EMP_FULL_TIME = 1
 
     emp_check = random.randint(0, 1)
-    if emp_check == emp_full_time:
+    if emp_check == EMP_FULL_TIME:
         print("Employee Present")
     else:
         print("Employee Absent")
 
+
+if __name__ == '__main__':
+    print("Welcome to Employee Wage Computation Program")
+    check_emp_present_absent()
