e3b874374d8ce42a08407895cf0aa4264f7a1434
prathameshadam10 <prathameshadam10@gmail.com>
2023-08-01 16:07:36 +0530

    added:By Using Switch case

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -2,23 +2,26 @@ import random
 
 EMP_FULL_TIME = 1
 EMP_PART_TIME = 2
+EMP_ABSENT = 0
 EMP_WAGE_PER_HOUR = 20
 
 
-def get_emp_hours(x):
-    emp_hrs = 0
-    if x == EMP_FULL_TIME:
-        emp_hrs = 8
-    elif x == EMP_PART_TIME:
-        emp_hrs = 4
-    else:
-        emp_hrs = 0
+def get_emp_hours():
+    switcher = {
+        EMP_FULL_TIME: 8,
+        EMP_PART_TIME: 4,
+        EMP_ABSENT: 0
+    }
+
+
+    emp_hrs = switcher.get(random.randint(0, 2))
+
     return emp_hrs
 
 
 def find_daily_wage():
-    y = get_emp_hours(random.randint(0, 2))
-    daily_emp_wage = y * EMP_WAGE_PER_HOUR
+    emp_hrs = get_emp_hours()
+    daily_emp_wage = emp_hrs * EMP_WAGE_PER_HOUR
     print('Emp Wage : ' + str(daily_emp_wage))
 
 
